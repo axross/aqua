c4d549d9f14dd1d6c28af98f3581eb178a090c60
Kohei Asai <me@axross.io>
2019-11-14 19:24:00 -0800

    Organizing dependencies via context around the main function

--- lib/common_widgets/app.dart
+++ lib/common_widgets/app.dart
@@ -3,51 +3,57 @@ import 'package:aqua/common_widgets/aqua_theme.dart';
 import 'package:aqua/pages/simulation_page/simulation_page.dart';
 import 'package:aqua/theme_data.dart';
 import 'package:aqua/view_models/simulation_session.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 
 class AquaApp extends StatefulWidget {
-  AquaApp({@required this.analytics, Key key})
-      : assert(analytics != null),
-        super(key: key);
-
-  final FirebaseAnalytics analytics;
-
   @override
   State<AquaApp> createState() => _AquaAppState();
 }
 
 class _AquaAppState extends State<AquaApp> {
-  SimulationSession _simulationSession;
+  ValueNotifier<SimulationSession> _simulationSession;
 
   @override
-  void initState() {
-    super.initState();
+  void didChangeDependencies() {
+    super.didChangeDependencies();
 
-    _simulationSession = SimulationSession.initial(
-      analytics: widget.analytics,
-    );
+    if (_simulationSession == null) {
+      _simulationSession = ValueNotifier(
+        SimulationSession.initial(
+          analytics: Analytics.of(context),
+        ),
+      );
+    }
   }
 
   @override
-  Widget build(BuildContext context) => Analytics(
-        analytics: widget.analytics,
-        child: AquaTheme(
-          lightThemeData: lightThemeData,
-          darkThemeData: darkThemeData,
-          child: SimulationSessionProvider(
-            simulationSession: _simulationSession,
-            child: WidgetsApp(
-              title: 'Odds Calculator',
-              color: Color(0xff19232e),
-              initialRoute: "/",
-              routes: {
-                "/": (_) => SimulationPage(),
-              },
-              pageRouteBuilder: <T>(settings, builder) =>
-                  MaterialPageRoute<T>(builder: builder, settings: settings),
-            ),
+  Widget build(BuildContext context) {
+    final analytics = Analytics.of(context);
+
+    return Analytics(
+      analytics: analytics,
+      child: AquaTheme(
+        lightThemeData: lightThemeData,
+        darkThemeData: darkThemeData,
+        child: ValueListenableBuilder<SimulationSession>(
+          valueListenable: _simulationSession,
+          builder: (context, simulationSession, child) =>
+              SimulationSessionProvider(
+            simulationSession: simulationSession,
+            child: child,
+          ),
+          child: WidgetsApp(
+            title: 'Odds Calculator',
+            color: Color(0xff19232e),
+            initialRoute: "/",
+            routes: {
+              "/": (_) => SimulationPage(),
+            },
+            pageRouteBuilder: <T>(settings, builder) =>
+                MaterialPageRoute<T>(builder: builder, settings: settings),
           ),
         ),
-      );
+      ),
+    );
+  }
 }
