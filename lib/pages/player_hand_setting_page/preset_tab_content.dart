0ada13259e630e95b6c43fa427e9f2dc3e5496fd
Kohei Asai <me@axross.io>
2019-11-15 16:33:42 -0800

    Bug fix: text colors for preset list were broken (#115)

--- lib/pages/player_hand_setting_page/preset_tab_content.dart
+++ lib/pages/player_hand_setting_page/preset_tab_content.dart
@@ -1,9 +1,12 @@
 import 'package:aqua/bundled_presets.dart';
 import 'package:aqua/common_widgets/aqua_theme.dart';
-import 'package:aqua/common_widgets/preset_list_item.dart';
+import 'package:aqua/common_widgets/readonly_range_grid.dart';
+import 'package:aqua/models/card_pair.dart';
 import 'package:aqua/models/player_hand_setting.dart';
 import 'package:aqua/models/player_hand_setting_preset.dart';
+import 'package:aqua/utilities/number_format.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 class PresetTabContent extends StatelessWidget {
@@ -33,7 +36,10 @@ class PresetTabContent extends StatelessWidget {
               children: [
                 Text(
                   "Reference Hand Ranges",
-                  style: theme.textStyle.copyWith(fontSize: 20),
+                  style: theme.textStyle.copyWith(
+                    fontSize: 20,
+                    color: theme.foregroundColor,
+                  ),
                 ),
                 SizedBox(height: 16),
                 _BundledPresetList(
@@ -66,11 +72,69 @@ class _BundledPresetList extends StatelessWidget {
         children: List.generate(
           bundledPresets.length * 2 - 1,
           (i) => i % 2 == 0
-              ? PresetListItem(
+              ? _PresetListItem(
                   preset: bundledPresets[i ~/ 2],
                   onTapped: () => onTapped(bundledPresets[i ~/ 2]),
                 )
               : SizedBox(height: 16),
         ),
       );
+}
+
+class _PresetListItem extends StatelessWidget {
+  _PresetListItem({@required this.preset, this.onTapped, Key key})
+      : assert(preset != null),
+        super(key: key);
+
+  final PlayerHandSettingPreset preset;
+
+  final void Function() onTapped;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = AquaTheme.of(context);
+    final playerHandSetting = preset.toPlayerHandSetting();
+
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        HapticFeedback.lightImpact();
+
+        if (onTapped != null) {
+          onTapped();
+        }
+      },
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 64,
+            height: 64,
+            child: ReadonlyRangeGrid(
+              handRange: playerHandSetting.onlyHandRange,
+            ),
+          ),
+          SizedBox(width: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 4),
+              Text(
+                preset.name,
+                style: theme.textStyle.copyWith(color: theme.foregroundColor),
+              ),
+              SizedBox(height: 4),
+              Text(
+                "${formatOnlyWholeNumberPart(playerHandSetting.cardPairCombinations.length / numberOfAllHoleCardCombinations)}% combs",
+                style: theme.textStyle.copyWith(
+                  color: theme.dimForegroundColor,
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
